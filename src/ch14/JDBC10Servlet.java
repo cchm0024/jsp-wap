93f8add4f99288450c0df918239df109086d1867
cchm0024 <cchm0024@naver.com>
2021-05-31 00:37:03 +0900

    20210528

--- src/ch14/JDBC10Servlet.java
+++ src/ch14/JDBC10Servlet.java
@@ -38,87 +38,84 @@ public class JDBC10Servlet extends HttpServlet {
 		
 		Customer customer = executeJDBC(id);
 		
-		request.setAttribute("costomer", customer);
+		request.setAttribute("customer", customer);
 		
 		String path = "/ch14/jdbc10.jsp";
 		request.getRequestDispatcher(path).forward(request, response);
-		
 	}
 	
-	
-private Customer executeJDBC(String id) {
+	private Customer executeJDBC(String id) {
 
-	Customer customer = null; //리턴할 객체
-	
-	String sql = "SELECT CustomerName, City FROM Customers WHERE CustomerID = " + id;
-
-	String url = "jdbc:mysql://54.180.160.140/test"; // 본인 ip
-	String user = "root";
-	String password = "wnddkdwjdqhcjfl1";
-
-	Connection con = null;
-	Statement stmt = null;
-	ResultSet rs = null;
-
-	try {
-		// 클래스 로딩
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		// 연결
-		con = DriverManager.getConnection(url, user, password);
-
-		// statement 생성
-		stmt = con.createStatement();
-
-		// 쿼리 실행, 결과(ResultSet) 리턴
-		rs = stmt.executeQuery(sql);
-
-		// 결과 탐색
-		if (rs.next()) {
-			String name = rs.getString(1);
-			String city = rs.getString(2);
+		Customer customer = null; // 리턴할 객체
 		
-			customer = new Customer();
-			customer.setName(name);
-			customer.setCity(city);
-			
-		}
+		String sql = "SELECT CustomerName, City FROM Customers WHERE CustomerID = " + id;
 
-	} catch (Exception e) {
-		e.printStackTrace();
-	} finally {
-		// 연결 닫기
-		if (rs != null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		String url = "jdbc:mysql://54.180.160.140/test"; // 본인 ip
+		String user = "root";
+		String password = "wnddkdwjdqhcjfl1";
+
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			// 클래스 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 연결
+			con = DriverManager.getConnection(url, user, password);
+
+			// statement 생성
+			stmt = con.createStatement();
+
+			// 쿼리 실행, 결과(ResultSet) 리턴
+			rs = stmt.executeQuery(sql);
+
+			// 결과 탐색
+			if (rs.next()) {
+				String name = rs.getString(1);
+				String city = rs.getString(2);
+				
+				customer = new Customer();
+				customer.setName(name);
+				customer.setCity(city);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 연결 닫기
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 
-		if (stmt != null) {
-			try {
-				stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		return customer;
 
-		if (con != null) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 	}
-
-	return customer;
-
-}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
